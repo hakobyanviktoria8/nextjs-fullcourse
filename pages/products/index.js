291f16b92b6986855912acbd8a2edf2f65fd4a55
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-03-22 22:43:34 +0400

    added json-server npm and lesten 4000 port

---
+++ pages/products/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div>
+        <h1>All products</h1>
+    </div>
+  )
+}
+
+export default Products
+
+// export async function getStaticProps(){
+    // const res = await fetch("l")
+// }
